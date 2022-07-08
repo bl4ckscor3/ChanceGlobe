b46ddfcb93f64c963234441ee6d67c3c91905bb9
bl4ckscor3 <bl4ckscor33@gmail.com>
2022-07-08 20:57:46 +0200

    use new features of forge 41.0.64
    also use the correct way to register block entity renderers

--- src/main/java/bl4ckscor3/mod/chanceglobe/client/ClientReg.java
+++ src/main/java/bl4ckscor3/mod/chanceglobe/client/ClientReg.java
@@ -1,22 +1,18 @@
 package bl4ckscor3.mod.chanceglobe.client;
 
 import bl4ckscor3.mod.chanceglobe.ChanceGlobe;
-import net.minecraft.client.renderer.ItemBlockRenderTypes;
-import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 @EventBusSubscriber(modid=ChanceGlobe.MODID, value=Dist.CLIENT, bus=Bus.MOD)
 public class ClientReg
 {
 	@SubscribeEvent
-	public static void onFMLClientSetup(FMLClientSetupEvent event)
+	public static void onEntityRenderersRegisterRenderers(EntityRenderersEvent.RegisterRenderers event)
 	{
-		event.enqueueWork(() ->	BlockEntityRenderers.register(ChanceGlobe.CHANCE_GLOBE_BLOCK_ENTITY.get(), ChanceGlobeTileEntityRenderer::new));
-		ItemBlockRenderTypes.setRenderLayer(ChanceGlobe.CHANCE_GLOBE.get(), RenderType.cutoutMipped());
+		event.registerBlockEntityRenderer(ChanceGlobe.CHANCE_GLOBE_BLOCK_ENTITY.get(), ChanceGlobeTileEntityRenderer::new);
 	}
 }
