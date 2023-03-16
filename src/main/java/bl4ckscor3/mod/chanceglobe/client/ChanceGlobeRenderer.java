aeec643a163b756276d3a45e5f836f29f6e93047
bl4ckscor3 <bl4ckscor33@gmail.com>
2023-03-16 15:08:43 +0100

    update to 1.19.4

--- src/main/java/bl4ckscor3/mod/chanceglobe/client/ChanceGlobeRenderer.java
+++ src/main/java/bl4ckscor3/mod/chanceglobe/client/ChanceGlobeRenderer.java
@@ -5,23 +5,25 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import bl4ckscor3.mod.chanceglobe.block.ChanceGlobeBlockEntity;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.block.model.ItemTransforms.TransformType;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.world.item.ItemDisplayContext;
 
 public class ChanceGlobeRenderer implements BlockEntityRenderer<ChanceGlobeBlockEntity> {
 	public ChanceGlobeRenderer(BlockEntityRendererProvider.Context ctx) {}
 
 	@Override
-	public void render(ChanceGlobeBlockEntity te, float partialTicks, PoseStack stack, MultiBufferSource buffer, int p_225616_5_, int p_225616_6_) {
+	public void render(ChanceGlobeBlockEntity te, float partialTicks, PoseStack stack, MultiBufferSource buffer, int light, int overlay) {
 		if (te.getClientItem().isEmpty())
 			return;
 
-		BakedModel model = Minecraft.getInstance().getItemRenderer().getModel(te.getClientItem(), te.getLevel(), null, 0);
+		ItemRenderer itemRenderer = Minecraft.getInstance().getItemRenderer();
+		BakedModel model = itemRenderer.getModel(te.getClientItem(), te.getLevel(), null, 0);
 
 		stack.translate(0.5D, 0.5D, 0.5D);
 		stack.scale(0.4F, 0.4F, 0.4F);
-		Minecraft.getInstance().getItemRenderer().render(te.getClientItem(), TransformType.GROUND, false, stack, buffer, p_225616_5_, p_225616_6_, model);
+		itemRenderer.render(te.getClientItem(), ItemDisplayContext.GROUND, false, stack, buffer, light, overlay, model);
 	}
 }
