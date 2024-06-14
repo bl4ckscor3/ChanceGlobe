27322f805821e19d8c87588e9d26f7e264b4f61a
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-06-14 10:04:18 +0200

    update to 1.21

--- src/main/java/bl4ckscor3/mod/chanceglobe/datagen/BlockLootTableGenerator.java
+++ src/main/java/bl4ckscor3/mod/chanceglobe/datagen/BlockLootTableGenerator.java
@@ -3,14 +3,15 @@ package bl4ckscor3.mod.chanceglobe.datagen;
 import java.util.Set;
 
 import bl4ckscor3.mod.chanceglobe.ChanceGlobe;
+import net.minecraft.core.HolderLookup;
 import net.minecraft.data.loot.BlockLootSubProvider;
 import net.minecraft.world.flag.FeatureFlags;
 import net.minecraft.world.level.block.Block;
 import net.neoforged.neoforge.registries.DeferredHolder;
 
 public class BlockLootTableGenerator extends BlockLootSubProvider {
-	protected BlockLootTableGenerator() {
-		super(Set.of(), FeatureFlags.REGISTRY.allFlags());
+	protected BlockLootTableGenerator(HolderLookup.Provider lookupProvider) {
+		super(Set.of(), FeatureFlags.REGISTRY.allFlags(), lookupProvider);
 	}
 
 	@Override
--- src/main/java/bl4ckscor3/mod/chanceglobe/datagen/DataGenRegistrar.java
+++ src/main/java/bl4ckscor3/mod/chanceglobe/datagen/DataGenRegistrar.java
@@ -1,15 +1,22 @@
 package bl4ckscor3.mod.chanceglobe.datagen;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
 import bl4ckscor3.mod.chanceglobe.ChanceGlobe;
+import net.minecraft.DetectedVersion;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.loot.LootTableProvider;
 import net.minecraft.data.loot.LootTableProvider.SubProviderEntry;
+import net.minecraft.data.metadata.PackMetadataGenerator;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.metadata.pack.PackMetadataSection;
+import net.minecraft.util.InclusiveRange;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
@@ -29,5 +36,11 @@ public class DataGenRegistrar {
 		generator.addProvider(event.includeServer(), new BlockTagGenerator(output, lookupProvider, event.getExistingFileHelper()));
 		generator.addProvider(event.includeServer(), new LootTableProvider(output, Set.of(), List.of(new SubProviderEntry(BlockLootTableGenerator::new, LootContextParamSets.BLOCK)), lookupProvider));
 		generator.addProvider(event.includeServer(), new RecipeGenerator(output, lookupProvider));
+		//@formatter:off
+		generator.addProvider(true, new PackMetadataGenerator(output)
+                .add(PackMetadataSection.TYPE, new PackMetadataSection(Component.literal("Mod resources & data"),
+                        DetectedVersion.BUILT_IN.getPackVersion(PackType.CLIENT_RESOURCES),
+                        Optional.of(new InclusiveRange<>(0, Integer.MAX_VALUE)))));
+		//@formatter:on
 	}
 }
