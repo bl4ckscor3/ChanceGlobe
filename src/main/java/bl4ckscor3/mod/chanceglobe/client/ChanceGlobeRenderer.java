740d3e0d81ee2f21f3bbefdefdad4d1eb17ac450
bl4ckscor3 <bl4ckscor33@gmail.com>
2025-03-31 21:13:47 +0200

    Update to 1.21.5

--- src/main/java/bl4ckscor3/mod/chanceglobe/client/ChanceGlobeRenderer.java
+++ src/main/java/bl4ckscor3/mod/chanceglobe/client/ChanceGlobeRenderer.java
@@ -9,12 +9,13 @@ import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraft.world.phys.Vec3;
 
 public class ChanceGlobeRenderer implements BlockEntityRenderer<ChanceGlobeBlockEntity> {
 	public ChanceGlobeRenderer(BlockEntityRendererProvider.Context ctx) {}
 
 	@Override
-	public void render(ChanceGlobeBlockEntity te, float partialTicks, PoseStack stack, MultiBufferSource buffer, int light, int overlay) {
+	public void render(ChanceGlobeBlockEntity te, float partialTicks, PoseStack stack, MultiBufferSource buffer, int light, int overlay, Vec3 cameraPosition) {
 		if (te.getClientItem().isEmpty())
 			return;
 
