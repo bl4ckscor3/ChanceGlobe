9e7fd2beaea4ae5a30309023542ed32746e983bf
bl4ckscor3 <bl4ckscor33@gmail.com>
2025-06-17 20:09:13 +0200

    Update to 1.21.6

--- src/main/java/bl4ckscor3/mod/chanceglobe/datagen/BlockTagGenerator.java
+++ src/main/java/bl4ckscor3/mod/chanceglobe/datagen/BlockTagGenerator.java
@@ -15,6 +15,6 @@ public class BlockTagGenerator extends BlockTagsProvider {
 
 	@Override
 	protected void addTags(HolderLookup.Provider provider) {
-		tag(BlockTags.MINEABLE_WITH_AXE).add(ChanceGlobe.CHANCE_GLOBE.getKey());
+		tag(BlockTags.MINEABLE_WITH_AXE).add(ChanceGlobe.CHANCE_GLOBE.get());
 	}
 }
